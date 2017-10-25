c85e29ac1c9c8193aa80f334ac76a5512ec2f311
morgancj4 <MORGANCJ@etsu.edu>
2017-10-24 21:39:01 -0400

    Added remove friend feature.

--- SCAM/FriendsList.cs
+++ SCAM/FriendsList.cs
@@ -12,19 +12,37 @@ using Android.Widget;
 
 namespace SCAM
 {
+
+
     [Activity(Label = "FriendsList")]
     public class FriendsList : ListActivity
     {
+        private const int SendMessage = 2;
+        private const int RemoveFriend = 1;
+
+        private List<Student> availableStudents;
+
+        private Student demo;
+
+        public List<Student> AvailableStudents
+        {
+            get { return availableStudents; }
+            set { availableStudents = value; }
+        }
+
+        
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            demo = Student.getStudent();
 
-            Student demo = Student.getStudent();
-            List<Student> availableStudents = Helper.available;
+            FriendPosition = -1;
 
-
+            availableStudents = Helper.available;
 
             ListAdapter = new ArrayAdapter<Student>(this, Android.Resource.Layout.SimpleListItem1, demo.Friends);
+
+
             // Create your application here
             SetContentView(Resource.Layout.FriendsListLayout);
 
@@ -35,12 +53,56 @@ namespace SCAM
                 StartActivity(intent);
             };
 
+            RegisterForContextMenu(ListView);
+
+
         }
+
+        public override void OnCreateContextMenu(IContextMenu menu, View v, IContextMenuContextMenuInfo menuInfo)
+        {
+            base.OnCreateContextMenu(menu, v, menuInfo);
+
+            menu.SetHeaderTitle("Friend Options");
+            FriendPosition = ((AdapterView.AdapterContextMenuInfo)(menuInfo)).Position;
+
+            menu.Add(0, FriendsList.SendMessage, 0, "Send Message");
+            menu.Add(0, FriendsList.RemoveFriend, 0, "Remove");
+
+        }
+        public int FriendPosition { get; set; }
+        public override bool OnContextItemSelected(IMenuItem item)
+        {
+            ArrayAdapter<Student> a = ListAdapter as ArrayAdapter<Student>;
+            switch (item.ItemId)
+            {
+                case SendMessage:
+                    Toast.MakeText(this.ApplicationContext, "Direct messaging is not yet implemented", ToastLength.Short).Show();
+                    return true;
+                case RemoveFriend:
+                    if (FriendPosition < a.Count && FriendPosition >= 0)
+                    {
+                        demo.Friends.RemoveAt(FriendPosition);
+                        a.Remove(a.GetItem(FriendPosition));
+                        return true;
+                    }
+                    return false;
+                default:
+                    return base.OnContextItemSelected(item);
+            }
+        }
+
+
+
+
         protected override void OnResume()
         {
             base.OnResume();
             Student demo = Student.getStudent();
             ListAdapter = new ArrayAdapter<Student>(this, Android.Resource.Layout.SimpleListItem1, demo.Friends);
         }
+
+
+
+
     }
 }
